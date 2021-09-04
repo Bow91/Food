cd4060d8cb9cb704ae8c539b7bb2b5093d800666
Bow91 <dscatoha@gmail.com>
2021-09-04 16:10:19 +0300

    v 1.1.1 add Timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
-
+    // Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -38,4 +38,55 @@ window.addEventListener('DOMContentLoaded', () => {
     hideTabContent();
     showTabContent();
 
+    // Timer
+    const deadLine = '2021-11-31';
+
+    function getTimeRemaining(endtime) {
+        const time = Date.parse(endtime) - new Date(),
+              days = Math.floor(time / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(time / (1000 * 60 *60) % 24),
+              minutes = Math.floor(time / (1000 * 60) % 60),
+              seconds = Math.floor(time / 1000 % 60);
+
+        return {
+            time,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'), 
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+             
+        function updateClock() {
+            const timeRemaining = getTimeRemaining(endtime);
+            
+            days.textContent = getZero(timeRemaining.days);
+            hours.textContent = getZero(timeRemaining.hours);
+            minutes.textContent = getZero(timeRemaining.minutes);
+            seconds.textContent = getZero(timeRemaining.seconds);
+                       
+            if (timeRemaining.total < 0) {
+                clearInterval(timeInterval);
+            }            
+        }
+        updateClock();  
+    }
+ 
+    setClock('.timer', deadLine);
 });
