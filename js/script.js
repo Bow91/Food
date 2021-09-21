ac6ce6ee478413a13a14f7f84474d3e2e1234490
Bow91 <dsca_toha@gmail.com>
2021-09-21 16:34:07 +0300

    v 1.7.0 add Calc

--- js/script.js
+++ js/script.js
@@ -271,7 +271,7 @@ window.addEventListener('DOMContentLoaded', () => {
           width = window.getComputedStyle(slidesWrapper).width;
     
     let offset = 0;
-    const widthNumber = +width.slice(0, width.length - 2);
+    const widthNumber = +width.replace(/\D/g, '');
     currentSlider.textContent = '01';
 
     slidesField.style.width = 100 * slides.length + '%';
@@ -355,6 +355,74 @@ window.addEventListener('DOMContentLoaded', () => {
             tapSlide();
         });
     });
-});
 
+//  Calculator
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', height, weight, age, ratio = 1.375;
+
+    function calcTotal() {
+        if(!sex || !height || !weight || !age || !ratio) {
+            result.textContent = 'Нет данных';
+            return;
+        }
+
+        if(sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+        
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+    
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+    
+                e.target.classList.add(activeClass);
+                
+                calcTotal();
+            });
+        });
+    }
+
+    function getDinamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch(input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+       
+        calcTotal();
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    getDinamicInformation('#height');
+    getDinamicInformation('#weight');
+    getDinamicInformation('#age');
+
+});
 //npx json-server --watch db.json
